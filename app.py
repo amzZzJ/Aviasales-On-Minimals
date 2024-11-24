ce5ce867e5f14c0aff1a65a3ea17b5a96a074e19
Anna Popova <annpp2812@gmail.com>
2024-11-24 13:19:58 +0300

    add and templates changed

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Главная страница
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Страница поиска
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        destination = request.form['destination']
+        max_price = request.form['max_price']
+        # Логика обработки запроса
+        results = [
+            {"price": 4500, "link": "http://example.com/ticket1"},
+            {"price": 5000, "link": "http://example.com/ticket2"}
+        ]
+        return render_template('search.html', results=results)
+    return render_template('search.html', results=[])
+
+if __name__ == "__main__":
+    app.run(debug=True)
