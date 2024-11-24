6698c514e425d54fed1e05f293999efc54249c4b
«Amina <daminka07@gmail.com>
2024-11-24 19:18:35 +0300

    Add sign up & log in

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'  # SQLite база данных
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+with app.app_context():
+    db.create_all()
 
 # Главная страница
 @app.route('/')
@@ -10,6 +31,49 @@ def index():
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Проверка на уникальность
+        if User.query.filter_by(email=email).first():
+            flash('Пользователь с таким email уже существует.', 'error')
+            return redirect(url_for('signup'))
+
+        # Хэширование пароля
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        # Сохранение в базу данных
+        new_user = User(username=username, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Регистрация прошла успешно! Войдите в свой аккаунт.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        # Поиск пользователя по email
+        user = User.query.filter_by(email=email).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            flash('Вы успешно вошли!', 'success')
+            # Здесь можно сохранить пользователя в сессии
+            return redirect(url_for('profile'))  # Перенаправление на защищенную страницу
+        else:
+            flash('Неправильный email или пароль.', 'error')
+
+    return render_template('login.html')
 
 # Страница поиска
 @app.route('/search', methods=['GET', 'POST'])
@@ -53,11 +117,6 @@ def registration():
         return redirect(url_for('login'))  # Переход на страницу входа, например.
 
     return render_template('registration.html')
-
-# Страница входа (пример)
-@app.route('/login')
-def login():
-    return render_template('login.html')  # Отображение страницы входа
 
 if __name__ == "__main__":
     app.run(debug=True)
