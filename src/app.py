68394dd71d3c38a7d90401976dfbb9848a01ce13
«Amina <daminka07@gmail.com>
2024-12-21 23:36:10 +0300

    AAAAAAAA

--- src/app.py
+++ src/app.py
@@ -4,10 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from parser.parser_aeroflot import search_tickets
 
-app = Flask(__name__, template_folder='resources/templates')
+app = Flask(__name__, template_folder='templates')
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(base_dir, 'resources', 'user.db')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///user.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'anyakseniaamina'
 
